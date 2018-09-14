c613dab09066082d301748dd3e6459d612b9c779
mkerbleski <mkerbles@gmail.com>
2018-09-14 10:27:14 -0600

    structured app.js

--- da_jokes/src/App.js
+++ da_jokes/src/App.js
@@ -1,21 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Routes} from 'react';
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+
+import Register from './components/register';
+import Login from './components/login';
+import Jokes from './components/jokes';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+  
+  componentDidMount(){
+    console.log("CDM");
+  }
+
+  logout = event => {
+    console.log('logout')
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <AppDiv>
+          <h1>App Div</h1>
+          <div className="links">
+            <Link to="/register" onClick={this.logout}>Logout</Link>
+            <Link to="/register" >Register</Link>
+            <Link to="/login" >Login</Link>
+            <Link to="/jokes" >Jokes</Link>
+          </div>
+          <Routes path="/register" component={Register} />
+          <Routes path="/login" component={Login} />
+          <Routes path="/jokes" component={Jokes} />
+        </AppDiv>
       </div>
     );
   }
 }
 
 export default App;
+
+const AppDiv = styled.div`
+  background: black;
+  color: white;
+  height: 100vh;
+  width: 100vw;
+`;
